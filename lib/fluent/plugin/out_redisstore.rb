4b398544d26947860b90aacecf5b3209e115bff7
moaikids <moaikids@gmail.com>
2013-08-17 20:03:09 +0900

    adding 'fixed_key_value '

--- lib/fluent/plugin/out_redisstore.rb
+++ lib/fluent/plugin/out_redisstore.rb
@@ -1,7 +1,7 @@
 module Fluent
   class RedisOutput < BufferedOutput
     Fluent::Plugin.register_output('redisstore', self)
-    attr_reader :host, :port, :db_number, :redis, :timeout, :key_prefix, :key_suffix, :store_type, :key_name, :score_name, :value_name, :key_expire, :value_expire, :value_length, :list_order
+    attr_reader :host, :port, :db_number, :redis, :timeout, :key_prefix, :key_suffix, :store_type, :key_name, :fixed_key_value, :score_name, :value_name, :key_expire, :value_expire, :value_length, :list_order
 
     def initialize
       super
@@ -21,6 +21,7 @@ module Fluent
       @key_suffix = conf.has_key?('key_suffix') ? conf['key_suffix'] : ''
       @store_type = conf.has_key?('store_type') ? conf['store_type'] : 'zset'
       @key_name = conf['key_name']
+      @fixed_key_value = conf.has_key?('fixed_key_value') ? conf['fixed_key_value'] : nil
       @score_name = conf['score_name']
       @value_name = conf['value_name']
       @key_expire = conf.has_key?('key_expire') ? conf['key_expire'].to_i : -1
@@ -31,7 +32,6 @@ module Fluent
 
     def start
       super
-
       @redis = Redis.new(:host => @host, :port => @port, :timeout => @timeout,
                          :thread_safe => true, :db => @db_number)
     end
@@ -74,7 +74,11 @@ module Fluent
 
     def operation_for_zset(record)
       now = Time.now.to_i
-      k = traverse(record, @key_name).to_s
+      if @fixed_key_value
+        k = @fixed_key_value
+      else
+        k = traverse(record, @key_name).to_s
+      end
       if @score_name
         s = traverse(record, @score_name)
       else
@@ -83,14 +87,12 @@ module Fluent
       v = traverse(record, @value_name)
       sk = @key_prefix + k + @key_suffix
       
-      @redis.multi do
-        @redis.zadd sk , s, v
-        if @key_expire > 0
-          @redis.expire sk , @key_expire
-        end
-        if @value_expire > 0
-          @redis.zremrangebyscore sk , '-inf' , (now - @value_expire)
-        end
+      @redis.zadd sk , s, v
+      if @key_expire > 0
+        @redis.expire sk , @key_expire
+      end
+      if @value_expire > 0
+        @redis.zremrangebyscore sk , '-inf' , (now - @value_expire)
       end
       if @value_length > 0
         script = generate_zremrangebyrank_script(sk, @value_length)
@@ -99,7 +101,11 @@ module Fluent
     end
 
     def operation_for_set(record)
-      k = traverse(record, @key_name).to_s
+      if @fixed_key_value
+        k = @fixed_key_value
+      else
+        k = traverse(record, @key_name).to_s
+      end
       v = traverse(record, @value_name)
       sk = @key_prefix + k + @key_suffix
               
@@ -110,19 +116,21 @@ module Fluent
     end
 
     def operation_for_list(record)
-      k = traverse(record, @key_name).to_s
+      if @fixed_key_value
+        k = @fixed_key_value
+      else
+        k = traverse(record, @key_name).to_s
+      end
       v = traverse(record, @value_name)
       sk = @key_prefix + k + @key_suffix
 
-      @redis.multi do
-        if @list_order == 'asc'
-          @redis.rpush sk, v
-        else
-          @redis.lpush sk, v
-        end             
-        if @key_expire > 0
-          @redis.expire sk, @key_expire
-        end
+      if @list_order == 'asc'
+        @redis.rpush sk, v
+      else
+        @redis.lpush sk, v
+      end             
+      if @key_expire > 0
+        @redis.expire sk, @key_expire
       end
       if @value_length > 0
         script = generate_ltrim_script(sk, @value_length, @list_order)
@@ -131,15 +139,17 @@ module Fluent
     end
 
     def operation_for_string(record)
-      k = traverse(record, @key_name).to_s
+      if @fixed_key_value
+        k = @fixed_key_value
+      else
+        k = traverse(record, @key_name).to_s
+      end
       v = traverse(record, @value_name)
       sk = @key_prefix + k + @key_suffix
          
-      @redis.multi do        
-        @redis.set sk, v
-        if @key_expire > 0
-          @redis.expire sk, @key_expire
-        end
+      @redis.set sk, v
+      if @key_expire > 0
+        @redis.expire sk, @key_expire
       end
     end
 
